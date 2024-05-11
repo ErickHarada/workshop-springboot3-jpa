849f96932bde59a052e1a7a38e95029f5f461e46
Erick <eefeaga2000@gmail.com>
2024-05-11 11:59:59 -0300

    Exception Handling - delete

---
+++ course/src/main/java/com/aprendendojava/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.aprendendojava.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
